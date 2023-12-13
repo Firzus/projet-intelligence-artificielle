082c5f7ff9c96327cc6fb8bc478a9caa57e172a3
Fabien Marchand <ghost.fm65@gmail.com>
2023-12-13 11:33:44 +0100

    first state for state machine (movement)

--- Assets/Scripts/State Machine/State1.cs
+++ Assets/Scripts/State Machine/State1.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class State1 : BaseState // movement 
 {
+    public GameObject A, B;
+    bool loop = false;
+    private float speed = 2.0f;
     public override void EnterState(StateManager stateObject)
     {
-        Debug.Log("Entering State 1 ");
+        //Debug.Log("Entering State 1 ");
+        A = GameObject.Find("/Waypoint/A");
+        B = GameObject.Find("/Waypoint/B");
     }
 
     public override void UpdateState(StateManager stateObject)
     {
-        Debug.Log("Updating state 1");
+        if (loop == false)
+        {
+            PointA(stateObject);
+        }
+        else
+        {
+            PointB(stateObject);
+        }
         //switch state 
         //stateObject.SwitchState(stateObject.state2);
+    }
+
+    private void PointA(StateManager stateObject)
+    {
+        Vector3 destination = B.transform.position;
+        Vector3 newPos = Vector3.MoveTowards(stateObject.transform.position, destination, speed * Time.deltaTime);
+        stateObject.transform.position = newPos;
+        float distance = Vector3.Distance(stateObject.transform.position, destination);
+        if (distance == 0.0f)
+        {
+            loop = true;
+        }
+    }
+
+    private void PointB(StateManager stateObject)
+    {
+        Vector3 destination = A.transform.position;
+        Vector3 newPos = Vector3.MoveTowards(stateObject.transform.position, destination, speed * Time.deltaTime);
+        stateObject.transform.position = newPos;
+        float distance = Vector3.Distance(stateObject.transform.position, destination);
+        if (distance == 0.0f)
+        {
+            loop = false;
+        }
     }
 }
