b9fd459e2556afa6ae5db42f8e926afd8eecac17
Kyosuuke <cli@gaming.tech>
2023-12-13 19:23:06 +0100

    Midway for the behavior tree UI

--- Assets/Scripts/BehaviorTree/BTRunner.cs
+++ Assets/Scripts/BehaviorTree/BTRunner.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class BTRunner : MonoBehaviour
 {
+    public BehaviorTree tree;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tree = tree.Clone();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tree.Update();
     }
 }
--- Assets/Scripts/BehaviorTree/Node.cs
+++ Assets/Scripts/BehaviorTree/Node.cs
@@ -1,84 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-
-namespace BehaviorTree
+public abstract class Node : ScriptableObject
 {
-    public enum NodeState
+    public enum State
     {
-        RUNNING,
-        SUCCESS,
-        FAILURE
+        Running,
+        Success,
+        Failure
     }
 
-    public class Node
+    public State state = State.Running;
+    public bool started = false;
+    public string guid;
+    public Vector2 position;
+
+    public State Update()
     {
-        protected NodeState state;
-
-        public Node parent;
-        protected List<Node> children = new List<Node>();
-
-        private Dictionary<string, object> _dataContext = new Dictionary<string, object>();
-
-        public Node()
+        if(!started)
         {
-            parent = null;
+            OnStart();
+            started = true;
         }
 
-        public Node(List<Node> children)
+        state = OnUpdate();
+
+        if (state == State.Failure || state == State.Success)
         {
-            foreach (Node child in children)
-                _Attach(child);
+            OnStop();
+            started = false;
         }
 
-        private void _Attach(Node node)
-        {
-            node.parent = this;
-            children.Add(node);
-        }
-
-        public virtual NodeState Evaluate() => NodeState.FAILURE;
-
-        public void SetData(string key, object value)
-        {
-            _dataContext[key] = value;
-        }
-
-        public object GetData(string key)
-        {
-            object value = null;
-            if (_dataContext.TryGetValue(key, out value))
-                return value;
-
-            Node node = parent;
-            while (node != null)
-            {
-                value = node.GetData(key);
-                if (value != null)
-                    return value;
-                node = node.parent;
-            }
-            return null;
-        }
-
-        public bool ClearData(string key)
-        {
-            if (_dataContext.ContainsKey(key))
-            {
-                _dataContext.Remove(key);
-                return true;
-            }
-
-            Node node = parent;
-            while (node != null)
-            {
-                bool cleared = node.ClearData(key);
-                if (cleared)
-                    return true;
-                node = node.parent;
-            }
-            return false;
-        }
+        return state;
     }
+
+    public virtual Node Clone()
+    {
+        return Instantiate(this);
+    }
+
+    protected abstract void OnStart();
+    protected abstract void OnStop();
+    protected abstract State OnUpdate();
 }
-
