8cef01edbb7c1e4e50f17db19f48d0de0a692f33
Fabien <84633381+Zearcus@users.noreply.github.com>
2023-12-14 18:44:12 +0100

    adding attack state and moving attack state

--- Assets/Scripts/State Machine/EnemyStateMachine/EnemyMovingAttackState.cs
+++ Assets/Scripts/State Machine/EnemyStateMachine/EnemyMovingAttackState.cs
@@ -1,33 +1,27 @@
 using UnityEngine;
 
-public class EnemyChaseState : EnemyBaseState
+public class EnemyMovingAttackState : EnemyBaseState
 {
-   
     GameObject target;
     public override void EnterState(EnemyStateManager enemy)
     {
+        Debug.Log("Enter to Moving State");
         target = GameObject.Find("player");//player
     }
+
     public override void UpdateState(EnemyStateManager enemy)
     {
         Chase(enemy);
     }
+
     public override void OnTriggerEnter2D(EnemyStateManager enemy, Collider2D col)
     {
-        //switch state if trigger
-        Debug.Log("Trigger " + col.name);
-        if (col.name == "player" && enemy.name == "Simple Bullet")
-        {
-            enemy.SwitchState(enemy.attack);
-        }
-        else if(col.name == "player" && enemy.name == "Elite Bullet"){
-            enemy.SwitchState(enemy.moving);
-        }
+        throw new System.NotImplementedException();
     }
 
     private void Chase(EnemyStateManager enemy)
     {
-        float speed = 2.0f;
+        float speed = 4.0f;
         enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, target.transform.position, speed * Time.deltaTime);
     }
 }
