1a22c740591e9a6d7d25b3996c672b8e495a1a21
joachim-gabin <ejoachimgabin@gaming.tech>
2023-12-19 12:58:49 +0100

    add explosif barrel and fix destroyable object
    add explosif barrel and fix destroyable object

--- Assets/RedBarrelExplosion.cs
+++ Assets/RedBarrelExplosion.cs
@@ -1,9 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public class RedBarrelExplosion : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject _explosion;
+
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        //
+        if (collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("Explosion"))
+        {
+            GameObject a = Instantiate(_explosion, gameObject.transform.position, gameObject.transform.rotation);
+            a.transform.localScale = new Vector3(4,4,0);
+            animator.SetBool("isDestroyed", true);
+            Destroy(gameObject);
+        }
     }
+
+
 }
