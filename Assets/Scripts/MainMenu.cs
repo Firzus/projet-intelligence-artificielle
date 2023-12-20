f163848261b2a05ca4ce4dfb50caf4d72953ca1c
Firzus <lprieu@gaming.tech>
2023-12-21 00:12:37 +0100

    Sound + Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        AudioManager.Instance.PlayMusic("MenuTheme");
+    }
+
     public void StartGame()
     {
         GameManager.instance.LoadScene("Level");
+
+        AudioManager.Instance.PlaycSFX("ClicSimple");
+        AudioManager.Instance.musicSource.Stop();
     }
 }
